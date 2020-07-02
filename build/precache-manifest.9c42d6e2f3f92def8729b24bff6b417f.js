56b1e7d98c87bdae0030aec24e3c27a3d13150a8
alumezi <arbnorlumezi@gmail.com>
2020-07-02 19:23:25 +0200

    BUild.

---
+++ build/precache-manifest.9c42d6e2f3f92def8729b24bff6b417f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69ffd844257d453587582bde339ef6f4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1f89216d281a58d79ce0",
+    "url": "/static/js/2.5406c7ab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5406c7ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85888900bdce97bf1a15",
+    "url": "/static/js/main.baf7e702.chunk.js"
+  },
+  {
+    "revision": "37e42bc47805ba865915",
+    "url": "/static/js/runtime-main.fac6ad05.js"
+  }
+]);
---
+++ build/precache-manifest.ace549e2d7b184d1618974faa490b89c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4df2f27a95671a50b46cd7820654b6a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1f89216d281a58d79ce0",
+    "url": "/static/js/2.5406c7ab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5406c7ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "328171012978ca96e90f",
+    "url": "/static/js/main.7c6f91a0.chunk.js"
+  },
+  {
+    "revision": "37e42bc47805ba865915",
+    "url": "/static/js/runtime-main.fac6ad05.js"
+  }
+]);
