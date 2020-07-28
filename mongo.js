2b5053d8d0400ee388f5aa501f84fe4b11a8e919
alumezi <arbnorlumezi@gmail.com>
2020-07-28 22:39:04 +0200

    Finish 3.12.

--- mongo.js
+++ mongo.js
@@ -7,33 +7,44 @@ if (process.argv.length < 3) {
 
 const password = process.argv[2]
 
-const url =
-  `mongodb+srv://Arbnor:${password}@cluster0-nkcte.mongodb.net/note-app?retryWrites=true&w=majority`
+const url = `mongodb+srv://Arbnor:${password}@cluster0-nkcte.mongodb.net/note-app?retryWrites=true&w=majority`;
 
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
 
 const noteSchema = new mongoose.Schema({
-  content: String,
+  name: String,
+  number: String,
   date: Date,
   important: Boolean,
 })
 
 const Note = mongoose.model('Note', noteSchema)
 
+if (process.argv.length === 3) {
+  Note.find({}).then(result => {
+    console.log("phonebook:")
+    result.forEach(note => {
+      console.log(`${note.name} ${note.number}`)
+    })
+    mongoose.connection.close()
+    process.exit(1)
+  })
+  return
+}
+
 const note = new Note({
-  content: 'HTML is Easy',
+  name: process.argv[3],
+  number: process.argv[4],
   date: new Date(),
   important: true,
 })
 
-// note.save().then(result => {
-//   console.log('note saved!')
-//   mongoose.connection.close()
-// })
-
-Note.find({}).then(result => {
-  result.forEach(note => {
-    console.log(note)
+note.save().then(result => {
+  Note.find({}).then(result => {
+    result.forEach(note => {
+      console.log(`${note.name} ${note.number}`)
+    })
+    mongoose.connection.close()
   })
-  mongoose.connection.close()
 })
+
