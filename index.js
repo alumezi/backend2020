7894044c285e4d9ba0d9953dd54e18027d64e0b9
alumezi <arbnorlumezi@gmail.com>
2020-08-12 11:46:30 +0200

    Linting and prettier

--- index.js
+++ index.js
@@ -1,119 +1,137 @@
-require('dotenv').config();
-const express = require('express');
-const morgan = require('morgan');
-const cors = require('cors');
-const PhoneBook = require('./models/phonebook');
-const app = express();
-app.use(express.json());
-app.use(cors());
-app.use(express.static('build'));
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const PhoneBook = require('./models/phonebook')
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+app.use(express.static('build'))
 // app.use(morgan('tiny'));
-morgan.token('returnData', (request) => {
-    return request.body;
-});
+morgan.token('returnData', (request) => request.body)
 
-app.use(morgan((tokens, req, res) => {
-    return [
-        tokens.method(req, res),
-        tokens.url(req, res),
-        tokens.status(req, res),
-        tokens.res(req, res, 'content-length'), '-',
-        tokens['response-time'](req, res), 'ms',
-        JSON.stringify(tokens.returnData(req))
-    ].join(' ');
-}));
+app.use(
+    morgan((tokens, req, res) =>
+        [
+            tokens.method(req, res),
+            tokens.url(req, res),
+            tokens.status(req, res),
+            tokens.res(req, res, 'content-length'),
+            '-',
+            tokens['response-time'](req, res),
+            'ms',
+            JSON.stringify(tokens.returnData(req)),
+        ].join(' ')
+    )
+)
 
-app.get("/info", (request, response) => {
-    PhoneBook.countDocuments({}).then(count => {
-        response.status(200).json({ info: `Phonebook has info for ${count} people ${new Date()}` });
+app.get('/info', (request, response) => {
+    PhoneBook.countDocuments({}).then((count) => {
+        response.status(200).json({
+            info: `Phonebook has info for ${count} people ${new Date()}`,
+        })
     })
-});
+})
 
 app.get('/api/persons', (request, response, next) => {
-    PhoneBook.find({}).then(res => {
-        response.json(res);
-    }).catch(err => next(err));
-});
+    PhoneBook.find({})
+        .then((res) => {
+            response.json(res)
+        })
+        .catch((err) => next(err))
+})
 
 app.get('/api/persons/:id', (request, response, next) => {
-    PhoneBook.findById(request.params.id).then(res => {
-        response.json(res);
-    }).catch(err => next(err));
-});
+    PhoneBook.findById(request.params.id)
+        .then((res) => {
+            response.json(res)
+        })
+        .catch((err) => next(err))
+})
 
 app.delete('/api/persons/:id', (request, response, next) => {
-    PhoneBook.findByIdAndRemove(request.params.id).then(result => {
-        response.status(204).end();
-    }).catch(err => next(err));
-});
+    PhoneBook.findByIdAndRemove(request.params.id)
+        .then(() => {
+            response.status(204).end()
+        })
+        .catch((err) => next(err))
+})
 
 app.put('/api/persons/:id', (request, response, next) => {
-    PhoneBook.findByIdAndUpdate(request.params.id, { number: request.body.number }, { new: true, runValidators: true })
-        .then(updatedResource => {
-            response.json(updatedResource);
+    PhoneBook.findByIdAndUpdate(
+        request.params.id,
+        { number: request.body.number },
+        { new: true, runValidators: true }
+    )
+        .then((updatedResource) => {
+            response.json(updatedResource)
         })
-        .catch(error => next(error))
+        .catch((error) => next(error))
 })
 
 app.post('/api/persons', (request, response, next) => {
-    let resource = request.body;
+    const resource = request.body
 
     if (!resource.name && !resource.number) {
         return response.status(400).json({
-            error: "Body missing"
-        });
+            error: 'Body missing',
+        })
     }
 
     if (!resource.name) {
         return response.status(400).json({
-            error: "Name missing"
+            error: 'Name missing',
         })
     }
 
     if (!resource.number) {
         return response.status(400).json({
-            error: "Number missing"
+            error: 'Number missing',
         })
     }
 
     const phonebookEntry = new PhoneBook({
         date: new Date(),
-        ...resource
+        ...resource,
     })
 
-    phonebookEntry.save().then(savedEntry => {
-        response.json(savedEntry);
-    }).catch(error => next(error))
-});
+    return phonebookEntry
+        .save()
+        .then((savedEntry) => {
+            response.json(savedEntry)
+        })
+        .catch((error) => next(error))
+})
 
 app.post('/api/seed', (request, response) => {
-    let phonebook = [
+    const phonebook = [
         {
-            "name": "Arto Hs",
-            "number": "123-456"
+            name: 'Arto Hs',
+            number: '123-456',
         },
         {
-            "name": "Ada Lovelace",
-            "number": "39-44-5323523"
+            name: 'Ada Lovelace',
+            number: '39-44-5323523',
         },
         {
-            "name": "Dan Abramov",
-            "number": "123-56489-756"
+            name: 'Dan Abramov',
+            number: '123-56489-756',
         },
         {
-            "name": "Mary Poppendieck",
-            "number": "39-23-6423111"
+            name: 'Mary Poppendieck',
+            number: '39-23-6423111',
         },
         {
-            "name": "Arbnor Lumezi",
-            "number": "555-123-123"
-        }
-    ];
-    PhoneBook.collection.insert(phonebook, (err, res) => {
+            name: 'Arbnor Lumezi',
+            number: '555-123-123',
+        },
+    ]
+    PhoneBook.collection.insert(phonebook, (err) => {
         if (err) {
-            response.status(400).end();
+            response.status(400).end()
         } else {
-            response.status(200).send(`Database seeded`);
+            response.status(200).send(`Database seeded`)
         }
     })
 })
@@ -123,18 +141,21 @@ const unknownEndpoint = (request, response) => {
 }
 
 const errorHandler = (error, request, response, next) => {
-    if (error.name === "CastError") {
-        return response.status(400).send({ error: 'malformatted id' });
-    } else if (error.name === "ValidationError") {
-        return response.status(400).send({ error: error.message });
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
     }
-    next(error);
+
+    if (error.name === 'ValidationError') {
+        return response.status(400).send({ error: error.message })
+    }
+    return next(error)
 }
 
-app.use(unknownEndpoint);
-app.use(errorHandler);
+app.use(unknownEndpoint)
+app.use(errorHandler)
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
+    // eslint-disable-next-line no-console
     console.log(`Server running on port ${PORT}`)
-});
+})
--- models/phonebook.js
+++ models/phonebook.js
@@ -1,14 +1,22 @@
-const mongoose = require('mongoose');
-const uniqueValidator = require('mongoose-unique-validator');
+/* eslint-disable no-underscore-dangle */
+const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
 
-mongoose.set('useFindAndModify', false);
+mongoose.set('useFindAndModify', false)
 
-mongoose.connect(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(result => {
-        console.log("result", result)
+mongoose
+    .connect(process.env.DB_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then((result) => {
+        // eslint-disable-next-line no-console
+        console.log('result', result)
+        // eslint-disable-next-line no-console
         console.log('connected to MongoDB')
     })
     .catch((error) => {
+        // eslint-disable-next-line no-console
         console.log('error connecting to MongoDB:', error.message)
     })
 
@@ -17,23 +25,26 @@ const phoneBookSchema = new mongoose.Schema({
         type: String,
         minlength: 3,
         required: true,
-        unique: true
+        unique: true,
     },
     number: {
         type: String,
         minlength: 8,
-        required: true
-    }
+        required: true,
+    },
 })
 
 phoneBookSchema.set('toJSON', {
     transform: (document, returnedObject) => {
+        // eslint-disable-next-line no-param-reassign
         returnedObject.id = returnedObject._id.toString()
+        // eslint-disable-next-line no-param-reassign
         delete returnedObject._id
+        // eslint-disable-next-line no-param-reassign
         delete returnedObject.__v
-    }
+    },
 })
 
-phoneBookSchema.plugin(uniqueValidator);
+phoneBookSchema.plugin(uniqueValidator)
 
-module.exports = mongoose.model('phoneBook', phoneBookSchema);
+module.exports = mongoose.model('phoneBook', phoneBookSchema)
